77ed5e9bd594e61429fe1370a10bc912e74ca284
Danny-vD <dvduist@hotmail.nl>
2024-04-05 20:05:29 +0200

    Add menu code

---
+++ XP-GameJam-2024/Assets/1. Scripts/Menu/MenuFunctionality.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VDFramework;
+
+namespace Menu
+{
+	public class MenuFunctionality : BetterMonoBehaviour
+	{
+		public void Quit()
+		{
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.ExitPlaymode();
+			return;
+#endif
+			
+			Application.Quit();
+		}
+
+		public void LoadNextScene()
+		{
+			int nextIndex = SceneManager.GetActiveScene().buildIndex + 1;
+			SceneManager.LoadScene(nextIndex);
+		}
+
+		public void LoadScene(int buildIndex)
+		{
+			SceneManager.LoadScene(buildIndex);
+		}
+
+		public void LoadScene(int buildIndex, LoadSceneMode loadSceneMode)
+		{
+			SceneManager.LoadScene(buildIndex, loadSceneMode);
+		}
+	}
+}
