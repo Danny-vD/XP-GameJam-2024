64172b12fdc446f53614707895b9e7b43a104fa1
Danny-vD <dvduist@hotmail.nl>
2024-04-07 16:03:39 +0200

    Dragon spawner

---
+++ XP-GameJam-2024/Assets/1. Scripts/Dragon/DragonSpawner.cs
@@ -0,0 +1,28 @@
+using System;
+using Timer.Events;
+using UnityEngine;
+using VDFramework;
+
+namespace Dragon
+{
+    public class DragonSpawner : BetterMonoBehaviour
+    {
+        [SerializeField]
+        private GameObject dragonPrefab;
+        
+        private void Awake()
+        {
+            GameTimerExpiredEvent.AddListener(SpawnDragon);
+        }
+
+        private void OnDestroy()
+        {
+            GameTimerExpiredEvent.RemoveListener(SpawnDragon);
+        }
+
+        private void SpawnDragon()
+        {
+            Instantiate(dragonPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
